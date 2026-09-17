ce86e95e942a053c1a96c95f2d38728ed7a797a5
Lir <toxik3772@gmail.com>
2026-09-17 12:56:18 +0300

    chore: validate character definitions at startup

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './canvasLocale';
 import './characterBuildBias';
+import './characterDataValidation';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
