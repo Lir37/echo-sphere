f8d3ddc50f1074b387ab28cb3f5a8275f32f44d5
Lir <toxik3772@gmail.com>
2026-09-18 07:44:59 +0300

    fix: finalize sphere terminology and remove legacy tower wording

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './canvasLocale';
-import './characterBuildBias';
 import './characterDataValidation';
 import './index.css';
 
