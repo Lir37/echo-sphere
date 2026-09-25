a98a0d1a5cbddbcc4edcac60bdaf49383deaca5d
Lir <toxik3772@gmail.com>
2026-09-25 15:31:47 +0300

    fix: import Vec for Ghost Snap projection

--- src/MobileControls.tsx
+++ src/MobileControls.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { Pause, Zap } from 'lucide-react';
 import { ABILITIES, SPHERE_TYPES, type AbilityType, type SphereType } from './gameData';
-import { activateByKey, activateDash, getMaxSpheres, placeSphere, setSphereType, VERTICAL_SLICE_SPHERE_TYPES, type GameState, type SphereEntity } from './engine';
+import { activateByKey, activateDash, getMaxSpheres, placeSphere, setSphereType, VERTICAL_SLICE_SPHERE_TYPES, type GameState, type SphereEntity, type Vec } from './engine';
 import { CHARACTER_DEFS } from './characters';
 import { getAbilityDisplayName } from './sphereProgression';
 import { getCharacterFormation, getEngineerNetworkSpheres } from './characterRuntime';
